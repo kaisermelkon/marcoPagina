668fad01da749fe9c315c6132e8e8cbcaaa76c4d
kaisermelkon <kaisermelkon@gmail.com>
2020-01-25 21:13:29 -0400

    marcoPagina

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<marcoPagina'));
+app.use(express.static(__dirname + '/dist/marcoPagina'));
 
 app.get('/*', function(req,res) {
     
