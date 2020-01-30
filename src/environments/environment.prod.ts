39b8e12fc5e15cb7b8cc01d258a75f99f6f7cf67
kaisermelkon <kaisermelkon@gmail.com>
2020-01-29 23:00:16 -0400

    firebase to enviorenment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDCpkwf-oOQYaV7zAugkWJEL5hgYbEOe38",
+    authDomain: "marcopagina-f0390.firebaseapp.com",
+    databaseURL: "https://marcopagina-f0390.firebaseio.com",
+    projectId: "marcopagina-f0390",
+    storageBucket: "marcopagina-f0390.appspot.com",
+    messagingSenderId: "667414557420",
+    appId: "1:667414557420:web:04600c9ddd3f101b1febc9",
+  }
 };
